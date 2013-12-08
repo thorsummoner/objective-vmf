0b0dbdfafbfa49b8712bee85e1aa555a23f1542d
Dylan Grafmyre <thorsummoner@live.com>
2013-12-07 23:29:18 -0800

    Ported old python2/ttk class to python3/gtk3, wow that was easy ._.

--- objective-vmf.py
+++ objective-vmf.py
@@ -2,78 +2,42 @@
 import os, sys
 # User our projects 'lib' path.
 sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), 'lib')))
-# Give actionable messages on dependency errors.
 try:
-	import tkinter
-	from tkinter import ttk
+	from gi.repository import Gtk
 	import obj
 except ImportError as err:
-	err.msg_list = err.msg.split()
-	# If this error is not Exactly the one I expect, keep throwing it.
-	if len(err.msg_list) == 9 and\
-		err.msg_list[3] == "'_tkinter'," and\
-		err.msg_list[7] == 'python-tk':
-		print('\033[2m{0}\033[0m'.format(err))
-		print(str("The program '{0}' is currently not installed. You can install it by typing:\n"\
-				+ "sudo apt-get install {0} --yes").format('python3-tk'))
-		sys.exit()
-	raise err
+	raise err;
 	sys.exit()
-
 
 def main(argv):
 	if argv != [__file__]:
 		#TODO: implement cli 
 		sys.exit()
-	root = tkinter.Tk()
 
-# DEMO CODE
-	# style = ttk.Style()
-	# style.theme_settings("default", {
-	#    "TCombobox": {
-	#        "configure": {"padding": 5},
-	#        "map": {
-	#            "background": [("active", "green2"),
-	#                           ("!disabled", "green4")],
-	#            "fieldbackground": [("!disabled", "green3")],
-	#            "foreground": [("focus", "OliveDrab1"),
-	#                           ("!disabled", "OliveDrab2")]
-	#        }
-	#    }
-	# })
+	class ObjectiveVmf(Gtk.Window):
+		def __init__(self):
+			print('[Application] initializing')
+			Gtk.Window.__init__(self, title="Objective Vmf")
 
-	# # print(style.theme_names())
-	# # style.theme_use('classic')
+			self.button = Gtk.Button(label="Test")
+			self.button.connect("clicked", self.test)
+			self.add(self.button)
 
-	# combo = ttk.Combobox().pack()
+			print('[Application] initialized')
 
-	# root.mainloop()
-# OLD APPLICATION
-	# class Application(Frame):
-# 		def __init__(self, master=None):
-# 			print('[Application] initializing')
-# 			self.master = master
-# 			Frame.__init__(self, master)
-# 			self.pack()
-# 			self.createWidgets()
-# 			master.protocol("WM_DELETE_WINDOW", self.exit)
-# 			print('[Application] initialized')
+		def test(self, widget):
+			print('test')
+			self.exit(self)
 
-# 		def createWidgets(self):
-# 			self.quitButton = ttk.Button(self, text='Quit', command=self.quit)
-# 			self.quitButton.grid()
+		def exit(self, args=None, kwargs=None):
+			print('[Application] exit')
+			Gtk.main_quit(self, args, kwargs)
 
-# 			self.testButton = ttk.Button(self, text='Test', command=obj.load)
-# 			self.testButton.grid()
 
-# 		def exit(self):
-# 			print('[Application] exit')
-# 			self.master.destroy()
-# 			self.master.quit()
-
-# 	root = Tk()
-# 	app = Application(master=root)
-# 	app.mainloop()
+	win = ObjectiveVmf()
+	win.connect("delete-event", win.exit)
+	win.show_all()
+	Gtk.main()
 
 if __name__ == "__main__":
 	main(sys.argv)
