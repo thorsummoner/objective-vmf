10c0b5ce2ba67eee404db63ec25a5352cf2eb4ed
Dylan Grafmyre <thorsummoner@live.com>
2013-12-07 21:24:05 -0800

    Cleanup comment code

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ try:
 	from Tkinter import *
 	import ttk
 	import obj
+# Not explicit, This catches missing py-tk stacktrace and instruct corrective actions.
 except ImportError as err:
-	# help(err)
-	# print err
-	# print err.__str__().split()[7]
-	print ("The program '{0}' is currently not installed. You can install it by typing:\n"\
-		+ "sudo apt-get install {0}").format(err.__str__().split()[7])
+	try:
+		missing_package = err.__str__().split()[7];
+		print '\033[2m{0}\033[0m'.format(err)
+		print ("The program '{0}' is currently not installed. You can install it by typing:\n"\
+			+ "sudo apt-get install {0}").format(err.__str__().split()[7])
+	except IndexError:
+		raise err
 	sys.exit()
 
 
