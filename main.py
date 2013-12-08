6f43c52aa9c9585cacfeffb03703fd317c45373b
Dylan Grafmyre <thorsummoner@live.com>
2013-12-07 17:41:00 -0800

    Added debian-like missing dependency notifications

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python2
 import sys
-from Tkinter import *
-import ttk
-import obj
+try:
+	from Tkinter import *
+	import ttk
+	import obj
+except ImportError as err:
+	# help(err)
+	# print err
+	# print err.__str__().split()[7]
+	print ("The program '{0}' is currently not installed. You can install it by typing:\n"\
+		+ "sudo apt-get install {0}").format(err.__str__().split()[7])
+	sys.exit()
+
 
 def main(argv):
 	class Application(Frame):
